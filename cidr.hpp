31b7895b14c049be4c64475a40b8b7d383947598
mutsuki <mutsuki@momo86.net>
2018-11-06 09:20:03 +0900

    split is_subnet

--- cidr.hpp
+++ cidr.hpp
@@ -20,20 +20,21 @@ inline unsigned int get_ipv4_address_bits(const std::string ipv4_address){
 	ipv4_address_bits = (ipv4_address_bits << 8) | block;
 	return ipv4_address_bits;
 }
+inline bool is_subnet(const std::string base_ipv4_address, const unsigned int cidr, const std::string ipv4_address){
+	const auto base_bits = get_ipv4_address_bits(base_ipv4_address);
+	const auto target_bits = get_ipv4_address_bits(ipv4_address);
+	const unsigned int subnet_mask = (~0) << (32 - cidr);
+
+	return (base_bits & subnet_mask) == (target_bits & subnet_mask);
+}
 inline bool is_subnet(const std::string base_cidr, const std::string ipv4_address){
 	// base_cidr : xxx.yyy.zzz.www/cc
 	// slash_pos : position of slash
 	const auto slash_pos = base_cidr.find_first_of("/");
 	// cidr : cc
 	const auto cidr = std::stoi(base_cidr.substr(slash_pos+1, base_cidr.length() - slash_pos));
-	// cidr_bits : bits(xxx.yyy.zzz.www)
-	auto cidr_bits = get_ipv4_address_bits(base_cidr.substr(0, slash_pos));
-	// cidr_bits : bits(ipv4_address)
-	auto ipv4_address_bits = get_ipv4_address_bits(ipv4_address);
-	// subnetmask
-	const unsigned int subnet_mask = (~0) << (32 - cidr);
 
-	return (cidr_bits & subnet_mask) == (ipv4_address_bits & subnet_mask);
+	return is_subnet(base_cidr.substr(0, slash_pos), cidr, ipv4_address);
 }
 }
 }
